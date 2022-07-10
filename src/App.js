5e72c7c51d37e52db44ffb428520794fd11dd06c
Nabil Amin <nabil.m.amin@gmail.com>
2022-07-10 10:33:29 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState(['todos 1', 'todos 2'])
+  // object destructering above. useState comes in as an array, and our const is destructering it.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <> 
+    {/* the above <> is called a fragment. allows us to put two or more jsx elements in the return of a function */}
+    <TodoList todos = {todos} />
+    <input type='text' />
+    <button>Add ToDo</button>
+    <button>Clear ToDo</button>
+    <div>0 Left to do</div>
+    </>
+  )
 }
 
 export default App;
